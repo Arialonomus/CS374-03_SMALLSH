b46173ea831dde1396568875d0e2134cc5d32ba9
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-13 17:31:18 -0600

    Added "environment" code files to hold functions related to maintenance of the process environment

---
+++ headers/environment.h
@@ -0,0 +1,21 @@
+/* Program: CS 374 Assignment 03 - SMALLSH
+ *
+ * This file contains functions related to the maintenance of regularly
+ * updated environment variables
+ */
+
+#ifndef SMALLSH_ENVIRONMENT_H
+#define SMALLSH_ENVIRONMENT_H
+
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "expand.h"
+
+int set_bgpid(pid_t pid);
+
+int set_exitstatus(int status);
+
+#endif //SMALLSH_ENVIRONMENT_H
---
+++ source/environment.c
@@ -0,0 +1,21 @@
+#include "environment.h"
+
+int set_bgpid(pid_t pid)
+{
+    char* temp_str = NULL;
+    if(asprintf(&temp_str, "%jd", (intmax_t)pid) == -1) return -1;
+    if(setenv(expand("$!"), temp_str, 1) == -1) return -1;
+    free(temp_str);
+
+    return 0;
+}
+
+int set_exitstatus(int status)
+{
+    char* temp_str = NULL;
+    if(asprintf(&temp_str, "%d", status) == -1) return -1;
+    if(setenv(expand("$!"), temp_str, 1) == -1) return -1;
+    free(temp_str);
+
+    return 0;
+}
