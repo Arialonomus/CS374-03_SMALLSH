1ffe10e3f501a2427e159af99b381163a44d66ef
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-13 11:37:28 -0600

    Added "handlers.h" header for signal handling functions

---
+++ headers/handlers.h
@@ -0,0 +1,13 @@
+/* Program: CS 374 Assignment 03 - SMALLSH
+ *
+ * This file contains signal and exit handlers and any related functions
+ */
+
+#ifndef SMALLSH_HANDLERS_H
+#define SMALLSH_HANDLERS_H
+
+typedef void (*sighandler_t)(int);
+
+inline void sigint_handler(int sig) {}
+
+#endif //SMALLSH_HANDLERS_H
