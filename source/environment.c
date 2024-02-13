edfbf5a2d8beb4434cc8ecb415393cb40462dd35
Jacob Barber <barber.jacobervin@gmail.com>
2024-02-13 17:47:35 -0600

    Fixed bug where wrong parameter was being expanded

--- source/environment.c
+++ source/environment.c
@@ -14,7 +14,7 @@ int set_exitstatus(int status)
 {
     char* temp_str = NULL;
     if(asprintf(&temp_str, "%d", status) == -1) return -1;
-    if(setenv(expand("$!"), temp_str, 1) == -1) return -1;
+    if(setenv(expand("$?"), temp_str, 1) == -1) return -1;
     free(temp_str);
 
     return 0;
